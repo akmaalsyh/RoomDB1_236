ae1f9c79f417657b3853e616708eecdd308eb18b
akmaalsyh <taufansyah50@gmail.com>
2025-11-27 10:56:27 +0700

    membuat route untuk home

---
+++ app/src/main/java/com/example/roomdb1_236/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.roomdb1_236.view.route
+
+import com.example.roomdb1_236.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
