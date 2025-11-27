3a28704c4db63a17ea09992de50877b4a208815b
akmaalsyh <taufansyah50@gmail.com>
2025-11-27 10:31:17 +0700

    membuat container aplikasi dan dependensi

---
+++ app/src/main/java/com/example/roomdb1_236/repositori/ContainerApp.kt
@@ -0,0 +1,29 @@
+package com.example.roomdb1_236.repositori
+
+import android.app.Application
+import android.content.Context
+import com.example.roomdb1_236.room.DatabaseSiswa
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context) :
+    ContainerApp {
+        override val repositoriSiswa: RepositoriSiswa by lazy {
+            OfflineRepositoriSiswa(
+                DatabaseSiswa.getDatabase(context).siswaDao())
+        }
+    }
+
+class AplikasiSiswa : Application() {
+        /**
+         * AppContainer instance digunakan oleh kelas-kelas lainnya untuk mendapatkan dependensi
+         */
+        lateinit var containerApp: ContainerApp
+
+        override fun onCreate() {
+            super.onCreate()
+            containerApp = ContainerDataApp(this)
+        }
+}
