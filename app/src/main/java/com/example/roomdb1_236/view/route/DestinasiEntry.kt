f17027f3deeea303380e01e547f375ab82a38ebd
akmaalsyh <taufansyah50@gmail.com>
2025-11-27 10:51:56 +0700

    membuat destinasi entry siswa

---
+++ app/src/main/java/com/example/roomdb1_236/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.roomdb1_236.view.route
+
+import com.example.roomdb1_236.R
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
