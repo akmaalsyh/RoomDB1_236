ecc1a4e31c2f1bcff881c9d90736ec40ee04d8a6
akmaalsyh <taufansyah50@gmail.com>
2025-11-27 11:05:48 +0700

    membuat peta navigasi siswa

---
+++ app/src/main/java/com/example/roomdb1_236/view/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,37 @@
+package com.example.roomdb1_236.view.uicontroller
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.roomdb1_236.view.route.DestinasiEntry
+import com.example.roomdb1_236.view.route.DestinasiHome
+import java.lang.reflect.Modifier
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier) {
+    HostNavigasi(navController = navController)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController, startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) }
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+
+    }
+}
